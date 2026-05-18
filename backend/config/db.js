80c07f7df9d6012418d72ad5f57e8c26dc83525f
janhvigupta01 <janhvigupta5555@gmail.com>
2026-05-18 23:32:08 +0530

    committed

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,2 +1,11 @@
 import mongoose from "mongoose";
-const
+const connectDb =async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL)
+        console.log("Db connected successfully")
+
+    }
+    catch(error){
+        console.log("db failed");
+    }
+}
