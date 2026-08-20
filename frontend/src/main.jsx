468f3251c0a7ec03facac969ff42da55b0fb317c
janhvigupta01 <janhvigupta5555@gmail.com>
2026-08-20 23:53:57 +0530

    updating

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,14 @@ import { BrowserRouter } from 'react-router-dom'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import {Provider} from "react-redux"
+import { store } from '../redux/store'
 export const serverUrl="http://localhost:8000"
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>
   </BrowserRouter>
   
 )
